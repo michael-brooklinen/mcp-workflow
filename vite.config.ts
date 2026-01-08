d2a109aac350f0a3617d79422e47d4a9e20c134e
Michael Crawbuck-Mathews <michael@brooklinen.com>
2026-01-08 12:22:14 -0500

    vite config

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import shopify from 'vite-plugin-shopify';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    shopify({
+      sourceCodeDir: 'frontend',
+      entrypointsDir: 'frontend/entrypoints',
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './frontend'),
+      '~': path.resolve(__dirname, './node_modules'),
+    },
+  },
+  build: {
+    sourcemap: true,
+    target: 'esnext',
+    emptyOutDir: true,
+  },
+});
